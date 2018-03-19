b80137f52b94027cd09c7dea6df8716002b03b64
Simon Rasmussen <simon.d.rasmussen@gmail.com>
2018-03-19 18:28:31 +0100

    nodejs cropping (output added to "faces" folder

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 //Setup npm install --save @google-cloud/vision
+// npm install --save jimp
 
 
 // Imports the Google Cloud client library
@@ -12,25 +13,37 @@ const client = new vision.ImageAnnotatorClient();
 //const fileName = 'test1.jpg';
 const fileName = 'POC/CropDummy/test1.jpg';
 
+var Jimp = require("jimp");
+
 client
   .faceDetection(fileName)
   .then(results => {
     const faces = results[0].faceAnnotations;
 
     console.log('Faces:');
+
     faces.forEach((face, i) => {
-      console.log('______________________________');
-      console.log(`  Face #${i + 1}:`);
-      console.log('top right?');
-      console.log(`    x : ${face.boundingPoly.vertices[0].x} y: ${face.boundingPoly.vertices[0].y}`);
-      console.log('top left?');
-      console.log(`    x: ${face.boundingPoly.vertices[1].x} y: ${face.boundingPoly.vertices[1].y}`);
-      console.log('bottom right?');
-      console.log(`    x : ${face.boundingPoly.vertices[2].x} y: ${face.boundingPoly.vertices[2].y}`);
-      console.log('bottom left?');
-      console.log(`    x: ${face.boundingPoly.vertices[2].x} y: ${face.boundingPoly.vertices[2].y}`);
+      let x = face.boundingPoly.vertices[0].x;
+      let y = face.boundingPoly.vertices[0].y;
+      let w = face.boundingPoly.vertices[2].x - x;
+      let h = face.boundingPoly.vertices[2].y - y;
+      cropImage(fileName,"face"+ i, x,y,w,h);
       });
   })
   .catch(err => {
     console.error('ERROR:', err);
   });
+
+
+//Gets image, should write a new local file cropped to fit the faces
+function cropImage(src,newName, x1, y1, x2, y2) {
+  console.log("cropping");
+  console.log("DEBUG: x:" + x1 + " y :" + y1 + "\n w: " + x2 + " h: " + y2 );
+  Jimp.read(src, function (err, src) {
+    if (err) throw err;
+    src.crop( x1, y1, x2, y2 )
+    .write("faces/" + newName + ".jpg");
+
+
+});
+}
